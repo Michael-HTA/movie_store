dec95cb1bd17d6c7a4d94244028f268fe6abe490
Michael-HTA <hnthu.aung@gmail.com>
2025-01-06 22:38:14 +0630

    navbar

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard.index');
 })->name('dashboard');
+Route::get('/logout', function () {
+    return view('dashboard.index');
+})->name('logout');
